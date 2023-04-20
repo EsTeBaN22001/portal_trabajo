e4419fdfc9a9ef79638a9a93cc9db4774047df80
Esteban Redón <esteban1.redon2@gmail.com>
2023-04-20 11:36:25 -0300

    Se creó el diseño de la barra lateral del dashboard principal

--- views/layouts/layout.php
+++ views/layouts/layout.php
@@ -17,35 +17,49 @@
 
 <body>
 
-	<div class="header-container">
-		<header class="header container-sm">
-			<div class="logo-container">
-				<a href="<?= $_SERVER['HOST'] ?>/">
-					<p class="logo">Porta<span>LT</span>rabajo</p>
-				</a>
+	<div class="site-container">
+		<aside class="aside">
+			<div class="profile-info-container">
+				<div class="img-container">
+					<i class="fa-solid fa-user"></i>
+				</div>
+				<p class="username">Esteban Redón</p>
 			</div>
-			<div class="navbar-container">
-				<nav class="nav">
-					<?php if(isset($_SESSION['login']) && $_SESSION['login']): ?>
-						<a href="<?= $_SERVER['HOST']  ?>/profile" class="link">Perfil</a>
-						<a href="<?= $_SERVER['HOST']  ?>/logout" class="link">Cerrar sesión</a>
-						<?php else: ?>
-							<a href="<?= $_SERVER['HOST']  ?>/register" class="link">Registrate</a>
-							<a href="<?= $_SERVER['HOST']  ?>/login" class="link">Iniciar Sesión</a>
-					<?php endif ?>
-				</nav>
+			<div class="options-list">
+				<div class="option">
+					<div class="icon-container">
+						<i class="fa-solid fa-magnifying-glass"></i>
+					</div>
+					<p>Buscar empleo</p>
+				</div>
+				<div class="option">
+					<div class="icon-container">
+						<i class="fas fa-file-alt"></i>
+					</div>
+					<p>Mis postulaciones</p>
+				</div>
+				<div class="option">
+					<div class="icon-container">
+						<i class="fa-solid fa-plus"></i>
+					</div>
+					<p>Nuevo trabajo</p>
+				</div>
+				<div class="option">
+					<div class="icon-container">
+						<i class="fa-solid fa-address-card"></i>
+					</div>
+					<p>Perfil</p>
+				</div>
 			</div>
-		</header>
+			<div class="logout-container">
+				<i class="fa-solid fa-arrow-right-from-bracket"></i>
+				<p>Cerrar sesión</p>
+			</div>
+		</aside>
+		<main class="main">
+			<?php echo $content; ?>
+		</main>
 	</div>
-
-	<main>
-		<?php echo $content; ?>
-	</main>
-
-
-	<footer class="footer">
-		<p>&#169;Todos los derechos reservados por PortalTrabajo 2022</p>
-	</footer>
 
 	<!-- Font Awesome -->
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.2/js/all.min.js" integrity="sha512-8pHNiqTlsrRjVD4A/3va++W1sMbUHwWxxRPWNyVlql3T+Hgfd81Qc6FC5WMXDC+tSauxxzp1tgiAvSKFu1qIlA==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
