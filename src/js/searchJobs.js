c4e480247f5dad692a3f18407777c7b93d8cfde6
Esteban Redon <esteban1.redon2@gmail.com>
2023-11-09 00:40:57 -0300

    Se mejoró el código para buscar trabajos

--- src/js/searchJobs.js
+++ src/js/searchJobs.js
@@ -9,8 +9,18 @@ function searchJobs() {
   const jobCards = document.querySelectorAll('.card');
 
   jobCards.forEach(function (jobCard) {
-    const jobTitle = jobCard.querySelector('.title').textContent.toLowerCase();
-    if (jobTitle.includes(searchValue)) {
+    const jobTitleElement = jobCard.querySelector('.title');
+    
+    // Elimina caracteres de nueva línea y divide el título en palabras
+    const jobTitle = jobTitleElement.textContent.replace(/\n/g, '').toLowerCase();
+    const titleWords = jobTitle.trim().split(' ');
+
+    // Verifica si el cuadro de búsqueda está vacío o si alguna palabra del título comienza con la búsqueda
+    const isMatch = searchValue === '' || titleWords.some(function (word) {
+      return word.startsWith(searchValue);
+    });
+
+    if (isMatch) {
       jobCard.style.display = 'flex';
     } else {
       jobCard.style.display = 'none';
